03e09844066dd17da6f8bd1d6d9b1f180b4be9f8
ngthanhdat <46674716+ngthanhdatt@users.noreply.github.com>
2021-12-10 20:59:04 +0700

    change sh file

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 SOURCE="/var/lib/jenkins/workspace/SampleWebApp-Demo1"
 BUILD_DIR="/opt/"
 
-rm -rf /opt/apache-tomcat-9.0.50/webapps/*
+rm -rf ${BUILD_DIR}/apache-tomcat-9.0.50/webapps/*
 
 cp ${SOURCE}/SampleWebApp.war ${BUILD_DIR}/apache-tomcat-9.0.50/webapps/
 
