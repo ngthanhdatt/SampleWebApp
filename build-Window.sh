a8168fa3da803f48c9aba0d5cdd0c9a5c1fbc1a9
ngthanhdat <46674716+ngthanhdatt@users.noreply.github.com>
2021-12-13 22:03:48 +0700

    Update build-Window.sh

--- build-Window.sh
+++ build-Window.sh
@@ -4,6 +4,6 @@ SOURCE="C:\ProgramData\Jenkins\.jenkins\workspace\SampleWebApp-Demo1"
 BUILD_DIR="D:\apache-tomcat-9"
 
 
-rm -rf ${BUILD_DIR}\apache-tomcat-9.0.54\webapps\*
+rmdir /s /q ${BUILD_DIR}\apache-tomcat-9.0.54\webapps\*
 
-cp ${SOURCE}\SampleWebApp.war ${BUILD_DIR}\apache-tomcat-9.0.54\webapps\*
+copy ${SOURCE}\SampleWebApp.war ${BUILD_DIR}\apache-tomcat-9.0.54\webapps\*
