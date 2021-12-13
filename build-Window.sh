f68dfa3b0ec485c0552a785221973aab043255cb
ngthanhdat <46674716+ngthanhdatt@users.noreply.github.com>
2021-12-13 22:08:31 +0700

    Update build-Window.sh

--- build-Window.sh
+++ build-Window.sh
@@ -6,4 +6,4 @@ BUILD_DIR="D:\apache-tomcat-9"
 
 rmdir /s /q ${BUILD_DIR}\apache-tomcat-9.0.54\webapps\*
 
-copy ${SOURCE}\SampleWebApp.war ${BUILD_DIR}\apache-tomcat-9.0.54\webapps\*
+copy ${SOURCE}\SampleWebApp.war ${BUILD_DIR}\apache-tomcat-9.0.54\webapps\
