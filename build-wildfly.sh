639e2fa86d75dcab8803bfca5ff323bde6d1f2da
ngthanhdat <46674716+ngthanhdatt@users.noreply.github.com>
2021-12-21 22:50:52 +0700

    Update build-wildfly.sh

--- build-wildfly.sh
+++ build-wildfly.sh
@@ -1,5 +1,6 @@
 SOURCE="/var/lib/jenkins/workspace/wildfly-standalone-SampleWebApp"
 BUILD_DIR="/opt"
 
+chmod a+x build-wildfly.sh
 
 cp ${SOURCE}/SampleWebApp.war ${BUILD_DIR}/wildfly-26.0.0.Final/standalone/deployments/
